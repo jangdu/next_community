aee59ece5cff9ab04c46706ac4163a08ac8098b6
jangdu <jjd0324@gmail.com>
2023-10-28 15:35:32 +0900

    FEAT : /api/auth/signin 로그인 api 구현

--- community_server/src/routes/auth.ts
+++ community_server/src/routes/auth.ts
@@ -1,6 +1,9 @@
+import bcrypt from 'bcryptjs';
 import { Request, Response, Router } from 'express';
 import User from '../entities/User';
-import { validate } from 'class-validator';
+import { isEmpty, validate } from 'class-validator';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
 const mapErrors = (errors: Object[]) => {
   return errors.reduce((acc: any, error: any) => {
@@ -44,8 +47,56 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
+// 로그인 로직
+const signin = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    let errors: any = {};
+
+    if (isEmpty(username)) errors.username = '이름을 입력해주세요';
+    if (isEmpty(password)) errors.password = '비밀번호를 입력해주세요';
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    const user = await User.findOne({ where: { username } });
+
+    if (!user) {
+      return res.status(404).json({ username: '등록되지 않은 이름입니다.' });
+    }
+
+    const matchedPassword = await bcrypt.compare(password, user.password);
+
+    if (!matchedPassword) {
+      return res
+        .status(401)
+        .json({ password: '비밀번호가 일치하지 않습니다.' });
+    }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+    // 쿠키저장
+    res.set(
+      'Set-Cookie',
+      cookie.serialize('token', token, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/',
+      }),
+    );
+
+    return res.json({ user, token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+
 const router = Router();
 
 router.post('/register', register);
+router.post('/signin', signin);
 
 export default router;
