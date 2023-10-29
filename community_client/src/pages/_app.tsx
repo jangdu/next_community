3d379daed18773bd682125e8f99ab5919458fc8d
jangdu <jjd0324@gmail.com>
2023-10-29 22:02:57 +0900

    FEAT : navbar UI 및 로그아웃 기능 구현

--- community_client/src/pages/_app.tsx
+++ community_client/src/pages/_app.tsx
@@ -1,14 +1,24 @@
+import { NavBar } from '@/components/NavBar';
 import { AuthProvider } from '@/context/auth';
 import '@/styles/globals.css';
 import axios from 'axios';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api';
+  axios.defaults.withCredentials = true;
+
+  const { pathname } = useRouter();
+  const authRoutes = ['/register', '/login'];
+  const authRoute = authRoutes.includes(pathname);
 
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      {!authRoute && <NavBar />}
+      <div className={authRoute ? '' : 'pt-12 bg-gray-200 min-h-screen'}>
+        <Component {...pageProps} />
+      </div>
     </AuthProvider>
   );
 }
