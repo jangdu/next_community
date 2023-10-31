d23858e3d8c242254255f8eff3b042fc38362999
jangdu <jjd0324@gmail.com>
2023-11-01 00:39:39 +0900

    FEAT : Post 생성 페이지 UI 구현

--- community_client/src/components/Sidebar.tsx
+++ community_client/src/components/Sidebar.tsx
@@ -29,7 +29,7 @@ const SideBar = ({ community }: Props) => {
 
           {authenticated && (
             <div className="mx-0 my-2">
-              <Link href={`/communities/${community.name}/create`}>
+              <Link href={`/communities/${community.name}/createPost`}>
                 <button className="w-full p-2 text-sm text-white bg-gray-400 rounded">
                   포스트 생성
                 </button>
--- community_client/src/pages/communities/[community]/createPost.tsx
+++ community_client/src/pages/communities/[community]/createPost.tsx
@@ -1,5 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function createPost() {
-  return <div className=""></div>;
+export default function CreatePost() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const submitPost = () => {};
+
+  return (
+    <div className="flex flex-col justify-center pt16">
+      <div className="w-10/12 mx-auto md:w-96">
+        <div className="p-4 bgwhite rounded">
+          <h1 className="mb-3 text-lg">포스트 생성 페이지</h1>
+          <form onSubmit={submitPost}></form>
+          <div className="relative mb-2">
+            <input
+              type="text"
+              className="w-full px-3 py-2 border border-gray-400 rounded focus:outline-none focus:bg-blue-300"
+              placeholder="Title"
+              maxLength={20}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <div
+              style={{ top: 10, right: 10 }}
+              className="absolute mb-2 text-sm text-gray-500 select-none"
+            >
+              {title.trim().length}/20
+            </div>
+          </div>
+          <textarea
+            rows={4}
+            placeholder="포스트 내용"
+            className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:bg-blue-300"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <div className="flex justify-end">
+            <button
+              className="px-4 py-2 text-sm font-semibold text-white bg-gray-400 border rounded"
+              disabled={title.trim().length === 0}
+            >
+              글 생성하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
