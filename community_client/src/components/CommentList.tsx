77d186b7c471bbc8d59b6f55a9e0185df77bdb80
jangdu <jjd0324@gmail.com>
2023-11-02 22:33:26 +0900

    refactor : 중복되는 vote 기능 컴포넌트로 따로 빼서 관리

--- community_client/src/components/CommentList.tsx
+++ community_client/src/components/CommentList.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Comment, Community } from '@/types';
+import { Comment, Community, Post } from '@/types';
 import useSWR from 'swr';
 import Link from 'next/link';
 import dayjs from 'dayjs';
 import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import Vote from './Vote';
 
 interface CommentListProps {
   comments: Comment[];
   identifier: string | string[];
   slug: string | string[];
-  onClickVoteBtn: any;
+  commentMutate: Function;
+  post: Post;
 }
 
 export default function CommentList({
-  identifier,
-  slug,
+  post,
   comments,
-  onClickVoteBtn,
+  commentMutate,
 }: CommentListProps) {
   return (
     <div className="flex flex-col">
@@ -24,28 +25,7 @@ export default function CommentList({
         <div className="flex " key={comment.identifier}>
           <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
             {/* 좋아요 */}
-            <div
-              className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
-              onClick={() => onClickVoteBtn(1, comment)}
-            >
-              {comment.userVote === 1 ? (
-                <FaArrowUp className="text-red-500" />
-              ) : (
-                <FaArrowUp />
-              )}
-            </div>
-            <p className="text-xs font-bold">{comment.voteScore}</p>
-            {/* 싫어요 */}
-            <div
-              className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
-              onClick={() => onClickVoteBtn(-1, comment)}
-            >
-              {comment.userVote === -1 ? (
-                <FaArrowDown className="text-red-500" />
-              ) : (
-                <FaArrowDown />
-              )}
-            </div>
+            <Vote post={post} comment={comment} mutate={commentMutate} />
           </div>
           <div className="py-2 pr-2">
             <div className="mb-2 text-xs leading-none">
--- community_client/src/components/PostCard.tsx
+++ community_client/src/components/PostCard.tsx
@@ -1,12 +1,10 @@
-import { useAuthState } from '@/context/auth';
-import { Comment, Community, Post } from '@/types';
-import axios from 'axios';
+import { Community, Post } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import React from 'react';
-import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import { FaRegCommentAlt } from 'react-icons/fa';
 import dayjs from 'dayjs';
+import Vote from './Vote';
 
 interface PostCardProps {
   post: Post;
@@ -21,61 +19,13 @@ export default function PostCard({
   isInCommunityPage,
   community,
 }: PostCardProps) {
-  const router = useRouter();
-  const { authenticated } = useAuthState();
-
-  const onClickVoteBtn = async (value: number, comment?: Comment) => {
-    if (!authenticated) {
-      alert('로그인 후 이용이 가능합니다!');
-    }
-
-    if (
-      (!comment && value === post?.userVote) ||
-      (comment && comment.userVote === value)
-    ) {
-      value = 0;
-    }
-
-    try {
-      await axios.post('/votes', {
-        identifier: post.identifier,
-        slug: post.slug,
-        commentIdentifier: comment?.identifier,
-        value,
-      });
-      mutate();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
-    <div className="flex mb-4 bg-white rounded" id={post.identifier}>
+    <div
+      className="flex mb-4 bg-white border-b border-gray-300"
+      id={post.identifier}
+    >
       {/* 투표 기능 */}
-      <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
-        <div
-          className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
-          onClick={() => onClickVoteBtn(1)}
-        >
-          {post.userVote === 1 ? (
-            <FaArrowUp className="text-red-500" />
-          ) : (
-            <FaArrowUp />
-          )}
-        </div>
-        <p className="text-xs font-bold">{post.voteScore}</p>
-        <div
-          className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
-          onClick={() => onClickVoteBtn(-1)}
-        >
-          {post.userVote === -1 ? (
-            <FaArrowDown className="text-blue-500" />
-          ) : (
-            <FaArrowDown />
-          )}
-        </div>
-      </div>
-
+      <Vote post={post} mutate={mutate} />
       {/* { 포스트 } */}
       <div className="w-full p-3 flex flex-col">
         <div className="flex flex-row items-center">
@@ -117,16 +67,20 @@ export default function PostCard({
             </p>
           </div>
         </div>
-        <Link className="w-fit" href={post.url}>
-          <h1 className="my-1 text-lg w-fit font-medium">{post.title}</h1>
+        <Link className="mt-4 w-fit" href={post.url}>
+          <h1 className=" hover:underline text-lg w-fit font-medium">
+            {post.title}
+          </h1>
         </Link>
         {post.body && <p className="my-1 text-sm">{post.body}</p>}
         <Link
-          className="ms-2 my-2 flex flex-row items-center gap-1"
+          className="ms-2 my-2 w-fit flex flex-row items-center gap-1 hover:text-gray-500"
           href={post.url}
         >
-          <i className="mr-1 fas fa-comment-alt fa-xs"></i>
-          <span>{post.commentCount || 0}</span>
+          <div className="text-lg mt-[3.2px] mr-1">
+            <FaRegCommentAlt />
+          </div>
+          <span>댓글 {post.commentCount || 0}</span>
         </Link>
       </div>
     </div>
