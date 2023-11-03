f4a85f15781cdfa5c85d98753ee4ffad154feb89
jangdu <jjd0324@gmail.com>
2023-11-04 01:07:55 +0900

    chore : typeorm logger false

--- community_server/src/data-source.ts
+++ community_server/src/data-source.ts
@@ -9,7 +9,7 @@ export const AppDataSource = new DataSource({
   password: 'password',
   database: 'postgres',
   synchronize: true,
-  logging: true,
+  logging: false,
   entities: ['src/entities/**/*.ts'],
   migrations: [],
   subscribers: [],
