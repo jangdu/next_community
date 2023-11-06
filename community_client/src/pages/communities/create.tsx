e17b34790077ea9bf68a6a31ebf0a3f6c81b317c
jangdu <jjd0324@gmail.com>
2023-11-07 01:11:21 +0900

    design : 포스트카드 및 네브바 app.tsx 전체적인 ui 수정

--- community_client/src/pages/communities/create.tsx
+++ community_client/src/pages/communities/create.tsx
@@ -18,7 +18,7 @@ export default function CommunityCreate() {
         { withCredentials: true },
       );
 
-      alert(`${res.data.name} 커뮤니티 생성이 완료되었습니다.`);
+      alert(`${res.data.name} 카테고리 생성이 완료되었습니다.`);
     } catch (error: any) {
       console.error(error);
       if ('response' in error && error.response?.data) {
@@ -30,42 +30,42 @@ export default function CommunityCreate() {
   };
 
   return (
-    <div className="flex flex-col justify-center pt-16">
-      <div className="w-10/12 mx-auto md:w-96">
-        <h1 className="mb-2 text-lg font-medium">커뮤니티 생성하기</h1>
+    <div className="flex flex-col justify-center bg-white mt-8 rounded-lg border shadow-lg">
+      <div className="w-10/12 mx-auto md:w-96 py-16">
+        <h1 className="mb-2 text-lg font-medium">카테고리 생성하기</h1>
         <hr />
         <form onSubmit={handleSubmit}>
           <div className="my-6">
-            <p className="font-medium">커뮤니티 명</p>
+            <p className="font-medium">카테고리 명</p>
             <p className="mb-2 text-xs text-gray-400">
               커뮤티니는 소문자로만 생성 할 수 있습니다.
             </p>
             <InputGroup
-              placeholder="커뮤니티 명"
+              placeholder="카테고리 명"
               value={name}
               setValue={setName}
               error={errors.name}
             />
           </div>
           <div className="my-6">
-            <p className="font-medium">커뮤니티 제목</p>
+            <p className="font-medium">카테고리 제목</p>
             <p className="mb-2 text-xs text-gray-400">
-              커뮤니티의 주가되는 주제로, 언제든지 변경이 가능합니다.
+              카테고리의 주가되는 주제로, 언제든지 변경이 가능합니다.
             </p>
             <InputGroup
-              placeholder="커뮤니티 주제"
+              placeholder="카테고리 주제"
               value={title}
               setValue={setTitle}
               error={errors.title}
             />
           </div>
           <div className="my-6">
-            <p className="font-medium">커뮤니티 설명</p>
+            <p className="font-medium">카테고리 설명</p>
             <p className="mb-2 text-xs text-gray-400">
-              커뮤니티에 대한 설명을 작성해주세요
+              카테고리에 대한 설명을 작성해주세요
             </p>
             <InputGroup
-              placeholder="커뮤니티 설명"
+              placeholder="카테고리 설명"
               value={description}
               setValue={setDescription}
               error={errors.description}
